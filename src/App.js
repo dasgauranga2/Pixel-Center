5cd935d9b7057bae55bc1f51f3e75813b60a227d
Gauranga Das <gaurangadas@Gaurangas-MacBook-Air.local>
2022-08-03 12:39:39 +0530

    Basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import the components
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <>
+		{/* render a component and */}
+		{/* use props to pass data to the component  */}
+		<Header text='HELLO WORLD' />
+	</>
 }
 
 export default App;
